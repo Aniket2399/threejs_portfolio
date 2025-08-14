af4307a1682439f78bd83aef8d863f2fdad3ba62
Aniket Kshirsagar <kshirsagarask19@gmail.com>
2025-08-14 14:07:51 -0400

    contact_changes

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -43,65 +43,70 @@ const Contact = () => {
   }
 
   return (
-    <section id='contact' className='c-space my-20 pb-40'>
-      <div className='relative min-h-screen flex items-center justify-center flex-col'>
-        <img src='/images/terminal.png' alt='terminal background' className='absolute inset-0 min-h-screen'/>
-          <div className='contact-container'>
-            <h3 className='text-content text-3xl font-semibold'>Let's talk</h3>
-            <p className='text-lg text-white-600 mt-3'> Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
-            life, I’m here to help.</p>
-            <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col space-y-7'>
-              <label className='space-y-3'>
-                <span className='field-label'>Full Name</span>
-                <input 
-                  type='text' 
-                  name='name' 
-                  value={form.name} 
-                  onChange={handleChange} 
-                  className='field-input' 
-                  required
-                  placeholder='Enter your name'
-                  />
-              </label>
-              <label className='space-y-3'>
-                <span className='field-label'>Email</span>
-                <input 
-                  type='email' 
-                  name='email' 
-                  value={form.email} 
-                  onChange={handleChange} 
-                  className='field-input' 
-                  required
-                  placeholder='Enter your email'
-                  />
-              </label>
-              <label className='space-y-3'>
-                <span className='field-label'>Your Message</span>
-                <textarea 
-                  type='text' 
-                  name='message' 
-                  value={form.message} 
-                  onChange={handleChange} 
-                  className='field-input' 
-                  required
-                  rows={5}
-                  placeholder='Hi, I want to...'
-                  />
-              </label>
-              <button type='submit' >
-                <div className='cta-button group'>
-                  <div className='bg-circle' />
-                    <p className='text'>{loading ? 'Sending...' : 'Send Message'}</p>
-                    <div className='arrow-wrapper'>
-                      <img src='/images/arrow-down.svg' alt='arrow'/>
-                    </div>
-                </div>
-              </button>
-            </form>
+    <section id='contact' className='c-space my-20 px-4 md:px-20 pb-20'>
+    <div className='relative min-h-screen flex items-center justify-center flex-col'>
+    <img
+      src='/images/terminal.png'
+      alt='terminal background'
+      className='absolute inset-0 min-h-screen '
+      />
+    <div className='contact-container w-full max-w-3xl'>
+      <h3 className='text-content text-3xl font-semibold'>Let's talk</h3>
+      <p className='text-lg text-white-600 mt-3'>
+        Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to life, I’m here to help.
+      </p>
+      <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col space-y-7'>
+        <label className='space-y-3'>
+          <span className='field-label'>Full Name</span>
+          <input
+            type='text'
+            name='name'
+            value={form.name}
+            onChange={handleChange}
+            className='field-input'
+            required
+            placeholder='Enter your name'
+          />
+        </label>
+        <label className='space-y-3'>
+          <span className='field-label'>Email</span>
+          <input
+            type='email'
+            name='email'
+            value={form.email}
+            onChange={handleChange}
+            className='field-input'
+            required
+            placeholder='Enter your email'
+          />
+        </label>
+        <label className='space-y-3'>
+          <span className='field-label'>Your Message</span>
+          <textarea
+            name='message'
+            value={form.message}
+            onChange={handleChange}
+            className='field-input'
+            required
+            rows={5}
+            placeholder='Hi, I want to...'
+          />
+        </label>
+        <button type='submit'>
+          <div className='cta-button group'>
+            <div className='bg-circle' />
+            <p className='text'>{loading ? 'Sending...' : 'Send Message'}</p>
+            <div className='arrow-wrapper'>
+              <img src='/images/arrow-down.svg' alt='arrow'/>
+            </div>
           </div>
-      </div>
-      
-    </section>
+        </button>
+      </form>
+    </div>
+    
+  </div>
+</section>
+
   )
 }
 
